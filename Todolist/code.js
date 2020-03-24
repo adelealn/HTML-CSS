00fbc252532856f6822247b5f449db11781b4d86
Adele <adele.alloin@yahoo.fr>
2020-03-24 17:35:54 +0100

    fonction ajout d'item à la liste & début boutons delete

--- Todolist/code.js
+++ Todolist/code.js
@@ -2,7 +2,26 @@
 let date = document.getElementById("date")
 date.innerHTML=moment().format("MMM Do YY");
 
-/*Regarder check d'un element
-Barrer le texte correspondant*/
-document.getElementById("00000").style.textDecoration="line-through";
 
+add_item = () =>{
+    let value_to_add= document.getElementById("add").value;
+    if (value_to_add != ""){
+        document.querySelector("#form_check").insertAdjacentHTML(`beforeend`, 
+                            `<p>
+                                <label class="container">`+value_to_add+
+                                    `<input type="checkbox" name="action">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <img id="delete" src="./delete.png" alt="">
+                            </p>`);
+        document.getElementById("add").value ="";
+    }
+}
+
+let PLUS = document.querySelector("#plus");
+PLUS.addEventListener('click', add_item);
+/*
+let MOINS = document.querySelector("#delete");
+MOINS.addEventListener('click', delete_item);
+
+*/
