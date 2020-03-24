b05b3f99e8c4c219e74ed3031a52eb899f7bcf08
Adele <adele.alloin@yahoo.fr>
2020-03-24 14:00:46 +0100

    tp todolist in progress

---
+++ Todolist/code.js
@@ -0,0 +1,8 @@
+
+let date = document.getElementById("date")
+date.innerHTML=moment().format("MMM Do YY");
+
+/*Regarder check d'un element
+Barrer le texte correspondant*/
+document.getElementById("00000").style.textDecoration="line-through";
+
